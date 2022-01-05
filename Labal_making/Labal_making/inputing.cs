7a27f7818f68f37ee2703f8d8a9316c60cdeea54
LuticaCANARD <presan100@gmail.com>
2022-01-05 11:19:50 +0900

    0

---
+++ Labal_making/Labal_making/inputing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Labal_making
+{
+    internal partial class inputing
+    {
+        public string Got_now_url()
+        {
+            
+            string path = Path.GetDirectoryName(typeof(inputing).Assembly.Location).ToString();
+            return path;
+        }
+
+        public List<projn> got_list(string path_of_file)
+        {
+
+        }
+       
+    }
+}
---
+++ Labal_making/Labal_making/projn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labal_making
+{
+    internal partial class projn
+    {
+        public string name; public string prof_n; public int year; public string agency; public int many_Lazy; public int many_small;
+        public projn(string _name, string _prof_n, int _year, string _agency, int _many_Lazy, int _many_small)
+        {
+            name = _name;
+            prof_n = _prof_n;
+            year = _year;
+            agency = _agency;
+            many_Lazy = _many_Lazy;
+            many_small = _many_small;
+        }
+    }
+
+    }
+
