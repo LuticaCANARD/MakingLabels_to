a1d98f7f39e641b37b90541d799ad000e15907f9
LuticaCANARD <presan100@gmail.com>
2022-01-05 12:37:18 +0900

    아웃함수화 구현

--- Labal_making/Labal_making/Largeset.cs
+++ Labal_making/Labal_making/Largeset.cs
@@ -8,9 +8,29 @@ namespace Labal_making
 {
     internal class Largeset
     {
-        public List<projn.projn_non> Get_largelist(List<projn> alllist)
+        public List<projn.projn_non> Get_Anomimuslist(List<projn> alllist,bool smallmode)
         {
-
+            List < projn.projn_non > output = new List<projn.projn_non>();
+            for (int i = 0; i < alllist.Count; i++)
+            {
+                var checkpoint = alllist[i].many_Lazy;
+                if (smallmode==true)
+                {
+                    checkpoint = alllist[i].many_small;
+                }
+                if (checkpoint == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    projn.projn_non jx = new projn.projn_non(
+                        alllist[i].name, alllist[i].prof_n, alllist[i].year, alllist[i].agency
+                        );
+                    output.Add(jx);
+                }
+            }
+            return output;
         }
     }
 }
